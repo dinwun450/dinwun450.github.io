35f840b31761600f034eae064ffb013e9bf33086
dinwun450 <42019518+dinwun450@users.noreply.github.com>
2023-06-19 00:30:15 -0700

    Update test.js

--- JS/test.js
+++ JS/test.js
@@ -1,2 +1,8 @@
-import { hello } from 'https://dinwun450.github.io/test2.js';
-hello();
+// import { hello } from 'https://dinwun450.github.io/test2.js';
+// hello();
+
+import { routeFetchColor } from "https://dinwun450.github.io/JS/trcypdia_findroute.js";
+
+var agency = prompt("Enter Agency");
+var route = prompt("Enter Route:")
+routeFetchColor(agency, route);
