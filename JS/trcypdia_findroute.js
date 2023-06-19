9f6e6aabb3a43ca76825017de6741f3250d499c2
dinwun450 <42019518+dinwun450@users.noreply.github.com>
2023-06-19 00:04:14 -0700

    Add files via upload

---
+++ JS/trcypdia_findroute.js
@@ -0,0 +1,36 @@
+function routeFetchColor(agency, route) {
+    var routeColorCall = new XMLHttpRequest();
+    if (agency === "AC Transit") {
+        routeColorCall.open("GET", "https://dinwun450.github.io/TransitAssets/SFBayArea/ac_routes.json");
+        routeColorCall.onreadystatechange = function() {
+            if (routeColorCall.status === 200 && routeColorCall.readyState === 4) {
+                var lineStat = JSON.parse(routeColorCall.responseText);
+                var colorStat = "#" + lineStat[route].route_color;
+                console.log(colorStat);
+                
+                document.getElementById("para").innerHTML = colorStat;
+                document.getElementById("para").style.color = colorStat;
+            }
+        }
+        routeColorCall.send();
+    }
+    else if (agency === "Union City Transit") {
+        routeColorCall.open("GET", "https://dinwun450.github.io/TransitAssets/SFBayArea/uc_routes.json");
+        routeColorCall.onreadystatechange = function() {
+            if (routeColorCall.status === 200 && routeColorCall.readyState === 4) {
+                var lineStat = JSON.parse(routeColorCall.responseText);
+                var colorStat = "#" + lineStat[route].route_color;
+                console.log(colorStat);
+                
+                document.getElementById("para").innerHTML = colorStat;
+                document.getElementById("para").style.color = colorStat;
+            }
+        }
+        routeColorCall.send();
+    }
+    else {
+        alert("That agency probably wasn't the best fit of one of the routes...");
+    }
+}
+
+export {routeFetchColor};
