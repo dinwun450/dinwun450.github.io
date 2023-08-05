fa0cb36c42f0cd622e29b960072f90ed0bb335b0
dinwun450 <42019518+dinwun450@users.noreply.github.com>
2023-08-05 01:50:23 -0700

    Delete JS directory

--- JS/test.js
+++
@@ -1,8 +0,0 @@
-// import { hello } from 'https://dinwun450.github.io/test2.js';
-// hello();
-
-import { routeFetchColor } from "https://dinwun450.github.io/JS/trcypdia_findroute.js";
-
-var agency = prompt("Enter Agency");
-var route = prompt("Enter Route:")
-routeFetchColor(agency, route);
