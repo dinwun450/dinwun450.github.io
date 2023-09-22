207830af190418800ea9df66022f5d432093316f
Dino Wun <mightygboole1815@gmail.com>
2023-09-22 09:09:43 -0700

    divide the routes to metro and bus and cable cars.

--- Transcyclopedia/sanfrancisco/muni/muni.js
+++ Transcyclopedia/sanfrancisco/muni/muni.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    function showBartRoutes() {
+    function showMuniRoutes() {
         var routeCall = new XMLHttpRequest();
-        routeCall.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q8y-sfmta&limit=700")
+        routeCall.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q8y-sfmta&limit=700&route_type=3")
         routeCall.onreadystatechange = function() {
             if (routeCall.readyState === 4 && routeCall.status === 200) {
                 var routeResult = JSON.parse(routeCall.responseText);
@@ -30,5 +30,71 @@ window.onload = function() {
         }
         routeCall.send();
     }
-    showBartRoutes();
+    showMuniRoutes();
+
+    function showMuniMetroRoutes() {
+        var routeCall = new XMLHttpRequest();
+        routeCall.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q8y-sfmta&limit=700&route_type=0")
+        routeCall.onreadystatechange = function() {
+            if (routeCall.readyState === 4 && routeCall.status === 200) {
+                var routeResult = JSON.parse(routeCall.responseText);
+                console.log(routeResult);
+                
+                for (var i=0; i<routeResult.routes.length; i++) {
+                    var routeShort = routeResult.routes[i].route_short_name;
+                    var routeLong = routeResult.routes[i].route_long_name;
+                    var routeColor = routeResult.routes[i].route_color;
+                    var routeColorText = routeResult.routes[i].route_text_color;
+
+                    document.getElementById("line_metro").innerHTML = routeShort;
+                    document.getElementById("route_desc_metro").innerHTML = routeLong;
+                    document.getElementById("line_metro").style.backgroundColor = `#${routeColor}40`;
+                    document.getElementById("line_metro").style.border = `1px solid #${routeColor}`;
+                    document.getElementById("line_metro").style.color = `#${routeColorText}`;
+
+                    var cloneTheList = document.getElementById("each_line_metro");
+                    var listTBC = cloneTheList.cloneNode(true);
+                    document.getElementById("munimetrolines").appendChild(listTBC);
+                }
+
+                var linesAll = document.getElementById("munimetrolines").children;
+                document.querySelector("#munimetrolines").removeChild(linesAll[0]);
+            }
+        }
+        routeCall.send();
+    }
+    showMuniMetroRoutes();
+
+    function showMuniCableRoutes() {
+        var routeCall = new XMLHttpRequest();
+        routeCall.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q8y-sfmta&limit=700&route_type=5")
+        routeCall.onreadystatechange = function() {
+            if (routeCall.readyState === 4 && routeCall.status === 200) {
+                var routeResult = JSON.parse(routeCall.responseText);
+                console.log(routeResult);
+                
+                for (var i=0; i<routeResult.routes.length; i++) {
+                    var routeShort = routeResult.routes[i].route_short_name;
+                    var routeLong = routeResult.routes[i].route_long_name;
+                    var routeColor = routeResult.routes[i].route_color;
+                    var routeColorText = routeResult.routes[i].route_text_color;
+
+                    document.getElementById("line_cable").innerHTML = routeShort;
+                    document.getElementById("route_desc_cable").innerHTML = routeLong;
+                    document.getElementById("line_cable").style.backgroundColor = `#${routeColor}40`;
+                    document.getElementById("line_cable").style.border = `1px solid #${routeColor}`;
+                    document.getElementById("line_cable").style.color = `#${routeColorText}`;
+
+                    var cloneTheList = document.getElementById("each_line_cable");
+                    var listTBC = cloneTheList.cloneNode(true);
+                    document.getElementById("municablelines").appendChild(listTBC);
+                }
+
+                var linesAll = document.getElementById("municablelines").children;
+                document.querySelector("#municablelines").removeChild(linesAll[0]);
+            }
+        }
+        routeCall.send();
+    }
+    showMuniCableRoutes();
 }
