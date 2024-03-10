6f237c0d9a74a4b6cb15ceda26b1f4c08c229134
Dino Wun <mightygboole1815@gmail.com>
2024-03-09 19:47:58 -0800

    Add the agency JS file, then test it with HTML

---
+++ Transcyclopedia/sanfrancisco/bus/js_assets/bus_agencies_info.js
@@ -0,0 +1,67 @@
+function changeAgency(a) {
+    var agency = a;
+
+    switch (agency) {
+        case "actransit":
+            console.log(a);
+            break;
+        case "beartransit":
+            console.log(a);
+            break;
+        case "commutedotorg":
+            console.log(a);
+            break;
+        case "countyconnection":
+            console.log(a);
+            break;
+        case "dumbartonexpress":
+            console.log(a);
+            break;
+        case "fast":
+            console.log(a);
+            break;
+        case "ggt":
+            console.log(a);
+            break;
+        case "marintransit":
+            console.log(a);
+            break;
+        case "petalumatransit":
+            console.log(a);
+            break;
+        case "sanmateotransit":
+            console.log(a);
+            break;
+        case "santarosacitybus":
+            console.log(a);
+            break;
+        case "solanotransit":
+            console.log(a);
+            break;
+        case "sonomacountytransit":
+            console.log(a);
+            break;
+        case "stanfordshuttles":
+            console.log(a);
+            break;
+        case "trideltatransit":
+            console.log(a);
+            break;
+        case "uctransit":
+            console.log(a);
+            break;
+        case "vacavillecitycoach":
+            console.log(a);
+            break;
+        case "napatransit":
+            console.log(a);
+            break;
+        case "trivalleywheels":
+            console.log(a);
+            break;
+        default:
+            console.log("nothing.")
+    }
+
+    console.log("these are those agencies.")
+}
