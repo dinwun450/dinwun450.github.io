5954f5e5391005d0bdacf7b4dc4eb325e90546a5
Dino Wun <mightygboole1815@gmail.com>
2024-04-03 16:18:02 -0700

    Add-in Stanford Marguerite routes

--- Transcyclopedia/sanfrancisco/bus/js_assets/peninsula_map.js
+++ Transcyclopedia/sanfrancisco/bus/js_assets/peninsula_map.js
@@ -238,3 +238,119 @@ function SamTransCreation() {
     sam_call.send();
 }
 setTimeout(SamTransCreation, 14000);
+
+marguerite = [];
+function StanfordCreation() {
+    var stan_call = new XMLHttpRequest();
+    stan_call.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q9h-stanford~marguerite&limit=700&include_geometry=true");
+    stan_call.onreadystatechange = function() {
+        if (stan_call.readyState === 4 && stan_call.status === 200) {
+            var bus_route_outputs = JSON.parse(stan_call.responseText);
+            var agency_bus = bus_route_outputs.routes[0].agency.agency_name;
+
+            for (var b=0; b<bus_route_outputs.routes.length; b++) {
+                var all_bus_lines_name_short = bus_route_outputs.routes[b].route_short_name;
+                var all_bus_lines_name_long = bus_route_outputs.routes[b].route_long_name;
+                var all_bus_lines_color = bus_route_outputs.routes[b].route_color; 
+                var all_bus_lines_text_color = bus_route_outputs.routes[b].route_text_color;
+                var geometry_bus = bus_route_outputs.routes[b].geometry;
+
+                if (geometry_bus === null) {
+                    console.log("nope.")
+                }
+                else {
+                    marguerite.push({
+                        'type': 'Feature',
+                        'geometry': geometry_bus,
+                        'properties': {
+                            'color': `#${all_bus_lines_color}`,
+                            'text_color': `#${all_bus_lines_text_color}`,
+                            'route_short_name': all_bus_lines_name_short,
+                            'route_long_name': all_bus_lines_name_long,
+                            'label': agency_bus
+                        }
+                    })
+                }
+            }
+
+            map.addSource('stanford_routes', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': marguerite
+                },
+                'generateId': true
+            });
+            console.log("loaded!")
+
+            map.addLayer({
+                'id': 'stanford',
+                'type': 'line',
+                'source': 'stanford_routes',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-width': 2,
+                    'line-color': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        ['get', 'color'],
+                        '#c0e7fc',
+                    ], //['get', 'color']
+                },
+            });
+
+            map.on('mouseenter', 'stanford', function(e) {
+                var fs = map.queryRenderedFeatures(e.point, { layers: ['stanford']});
+                console.log(fs)
+
+                if (fs.length > 0) {
+                    for (var f = 0; f < fs.length; f ++) {
+                        var name_of_route = fs[f].properties.route_short_name;
+                        routeId2.push(name_of_route)
+                        
+                        hoveredPolygonLine2 = fs[f].id;
+                        hoverIdBus2.push(hoveredPolygonLine2);
+
+                        if (hoveredPolygonLine2 !== null) {
+                            console.log("hello!")
+                            map.setFeatureState(
+                                { source: 'stanford_routes', id: hoveredPolygonLine2 },
+                                { hover: false }
+                            );
+                        }
+                        map.setFeatureState(
+                            { source: 'stanford_routes', id: hoveredPolygonLine2 },
+                            { hover: true }
+                        );
+                    }
+                    
+                    // Populate the popup and set its coordinates
+                    // based on the feature found.
+                    popup2.setLngLat(e.lngLat.wrap()).setHTML(routeId2).addTo(map);
+                }
+            });
+
+            map.on('mouseleave', 'stanford', (e) => {
+                if (routeId2.length > 0) {routeId2 = []}
+                popup2.remove();
+                // document.getElementById("range_of_routes").innerHTML = `<li class="route_radius"><span id="route_short">-</span> <span id="detailed_route">Loading...</span></li>`;
+
+                if (hoveredPolygonLine2 !== null) {
+                    console.log("Fix!")
+                    for (var i = 0; i < hoverIdBus2.length; i++) {
+                        map.setFeatureState(
+                            { source: 'stanford_routes', id: hoverIdBus2[i]},
+                            { hover: false }
+                        );
+                    }
+                }
+                hoveredPolygonLine2 = null;
+            });
+        }
+    }
+    stan_call.send();
+}
+setTimeout(StanfordCreation, 15000);
