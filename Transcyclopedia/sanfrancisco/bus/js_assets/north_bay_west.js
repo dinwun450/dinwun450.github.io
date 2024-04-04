0fb6cc9232bbde354daf1dcc13efc01d9bb953e9
Dino Wun <mightygboole1815@gmail.com>
2024-04-03 23:16:22 -0700

    Add Santa Rosa CityBus to map

--- Transcyclopedia/sanfrancisco/bus/js_assets/north_bay_west.js
+++ Transcyclopedia/sanfrancisco/bus/js_assets/north_bay_west.js
@@ -473,3 +473,122 @@ function PetalumaTransitCreation() {
     petaluma_call.send();
 }
 setTimeout(PetalumaTransitCreation, 19000);
+
+santa_rosa = [];
+function SantaRosaTransitCreation() {
+    var santarosa_call = new XMLHttpRequest();
+    santarosa_call.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9qbdx-santarosacitybus&limit=700&include_geometry=true");
+    santarosa_call.onreadystatechange = function() {
+        if (santarosa_call.readyState === 4 && santarosa_call.status === 200) {
+            var bus_route_outputs = JSON.parse(santarosa_call.responseText);
+            var agency_bus = bus_route_outputs.routes[0].agency.agency_name;
+
+            for (var b=0; b<bus_route_outputs.routes.length; b++) {
+                var all_bus_lines_name_short = bus_route_outputs.routes[b].route_short_name;
+                var all_bus_lines_name_long = bus_route_outputs.routes[b].route_long_name;
+                var all_bus_lines_color = bus_route_outputs.routes[b].route_color; 
+                var all_bus_lines_text_color = bus_route_outputs.routes[b].route_text_color;
+                var geometry_bus = bus_route_outputs.routes[b].geometry;
+
+                if (geometry_bus === null) {
+                    console.log("nope.")
+                }
+                else {
+                    santa_rosa.push({
+                        'type': 'Feature',
+                        'geometry': geometry_bus,
+                        'properties': {
+                            'color': `#${all_bus_lines_color}`,
+                            'text_color': `#${all_bus_lines_text_color}`,
+                            'route_short_name': all_bus_lines_name_short,
+                            'route_long_name': all_bus_lines_name_long,
+                            'label': agency_bus
+                        }
+                    })
+                }
+            }
+
+            map.addSource('santa_rosa_routes', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': santa_rosa
+                },
+                'generateId': true
+            });
+            console.log("loaded!")
+
+            map.addLayer({
+                'id': 'santa_rosa_city_bus',
+                'type': 'line',
+                'source': 'santa_rosa_routes',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-width': 2,
+                    'line-color': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        ['get', 'color'],
+                        '#c0e7fc',
+                    ], //['get', 'color']
+                },
+            });
+
+            map.on('mouseenter', 'santa_rosa_city_bus', function(e) {
+                var fs = map.queryRenderedFeatures(e.point, { layers: ['santa_rosa_city_bus']});
+                console.log(fs)
+
+                if (fs.length > 0) {
+                    for (var f = 0; f < fs.length; f ++) {
+                        var name_of_route = fs[f].properties.route_short_name;
+                        if (name_of_route === "") {
+                            name_of_route = fs[f].properties.route_long_name;
+                        }
+                        routeId.push(name_of_route)
+                        
+                        hoveredPolygonLine = fs[f].id;
+                        hoverIdBus.push(hoveredPolygonLine);
+
+                        if (hoveredPolygonLine !== null) {
+                            console.log("hello!")
+                            map.setFeatureState(
+                                { source: 'santa_rosa_routes', id: hoveredPolygonLine },
+                                { hover: false }
+                            );
+                        }
+                        map.setFeatureState(
+                            { source: 'santa_rosa_routes', id: hoveredPolygonLine },
+                            { hover: true }
+                        );
+                    }
+                    
+                    // Populate the popup and set its coordinates
+                    // based on the feature found.
+                    popup2.setLngLat(e.lngLat.wrap()).setHTML(routeId).addTo(map);
+                }
+            });
+
+            map.on('mouseleave', 'santa_rosa_city_bus', (e) => {
+                if (routeId.length > 0) {routeId = []}
+                popup2.remove();
+                // document.getElementById("range_of_routes").innerHTML = `<li class="route_radius"><span id="route_short">-</span> <span id="detailed_route">Loading...</span></li>`;
+
+                if (hoveredPolygonLine !== null) {
+                    console.log("Fix!")
+                    for (var i = 0; i < hoverIdBus.length; i++) {
+                        map.setFeatureState(
+                            { source: 'santa_rosa_routes', id: hoverIdBus[i]},
+                            { hover: false }
+                        );
+                    }
+                }
+                hoveredPolygonLine = null;
+            });
+        }
+    }
+    santarosa_call.send();
+}
+setTimeout(SantaRosaTransitCreation, 20000);
