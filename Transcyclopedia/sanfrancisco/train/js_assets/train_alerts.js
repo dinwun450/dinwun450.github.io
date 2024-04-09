34cfbb391e825d9b3b3f4f1d8bcffe6e17d0fc9a
Dino Wun <mightygboole1815@gmail.com>
2024-04-08 17:21:49 -0700

    Add Alerts function to alerts page

---
+++ Transcyclopedia/sanfrancisco/train/js_assets/train_alerts.js
@@ -0,0 +1,60 @@
+var agency_onestop_id = "";
+
+function changeAgencyInAlerts(d) {
+    var agency = d;
+
+    switch(agency) {
+        case "ace":
+            agency_onestop_id = "o-9q9-acealtamontcorridorexpress";
+            break;
+        case "caltrain":
+            agency_onestop_id = "o-9q9-caltrain";
+            break;
+        case "capitol_corridor":
+            agency_onestop_id = "o-9qc-capitolcorridor";
+            break;
+        case "smart":
+            agency_onestop_id = "o-9qb-sonomamarinarearailtransit";
+            break;
+        default:
+            agency_onestop_id = "";
+            break;
+    }
+
+    if (agency === "prompt") {
+        document.querySelector(".alert_entity").innerHTML = `
+        <p id="alert_desc">Select a train agency on the top.</p>
+        `;
+    }
+    else {
+        compileAlerts(agency_onestop_id);
+    }
+}
+
+function compileAlerts(oneStopAgency) {
+    document.querySelector(".alert_entity").innerHTML = `
+        <p id="alert_desc">Loading...</p>
+    `;
+
+    var alert_call = new XMLHttpRequest();
+    alert_call.open("GET", `https://transit.land/api/v2/rest/agencies?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&onestop_id=${oneStopAgency}&include_alerts=true`);
+    alert_call.onreadystatechange = function() {
+        if (alert_call.readyState === 4 && alert_call.status === 200) {
+            var alert_outputs = JSON.parse(alert_call.responseText).agencies[0].alerts;
+
+            for (var i=0; i<alert_outputs.length; i++) {
+                var header_text = alert_outputs[i].header_text[0].text;
+                var desc_text = alert_outputs[i].description_text[0].text;
+
+                document.getElementById("alert_desc").innerHTML = `<b>${header_text}</b><br>${desc_text}`;
+                var alertNode = document.getElementById("alert_desc");
+                var cloneAlert = alertNode.cloneNode(true);
+                document.querySelector(".alert_entity").appendChild(cloneAlert);
+            }
+
+            var all_alerts = document.querySelector(".alert_entity");
+            document.querySelector(".alert_entity").removeChild(all_alerts[0]);
+        }
+    }
+    alert_call.send();
+}
