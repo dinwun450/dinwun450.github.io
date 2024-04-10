1bb422e6fae91dbebb90833fc7e8aa1a1b362ffd
Dino Wun <mightygboole1815@gmail.com>
2024-04-10 07:31:57 -0700

    Add second part of a plotting map function

--- Transcyclopedia/sanfrancisco/train/js_assets/map_plotter_of_train_lines.js
+++ Transcyclopedia/sanfrancisco/train/js_assets/map_plotter_of_train_lines.js
@@ -142,5 +142,49 @@ function plotAllTrainLines() {
         'generateId': true
     });
     console.log("Loaded!");
+
+    map.on('style.load', () => {
+        // Insert the layer beneath any symbol layer.
+        const layers = map.getStyle().layers;
+        const labelLayerId = layers.find(
+            (layer) => layer.type === 'symbol' && layer.layout['text-field']
+        ).id;
+        
+        map.addLayer({
+                'id': 'add-3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                'fill-extrusion-color': '#aaa',
+                
+                // Use an 'interpolate' expression to
+                // add a smooth transition effect to
+                // the buildings as the user zooms in.
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        }),
+        labelLayerId
+    })
 }
 setTimeout(plotAllTrainLines, 5000)
