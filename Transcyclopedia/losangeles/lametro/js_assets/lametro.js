9d94cb1fdbf1ead8779fddf93211599ce91cce06
Dino Wun <mightygboole1815@gmail.com>
2024-04-28 17:27:33 -0700

    Add bus routes to lines page

--- Transcyclopedia/losangeles/lametro/js_assets/lametro.js
+++ Transcyclopedia/losangeles/lametro/js_assets/lametro.js
@@ -97,3 +97,46 @@ function loadLAMetroLinesLR() {
     route_fetcher.send();
 }
 loadLAMetroLinesLR();
+
+function loadLAMetroLinesBus() {
+    var route_fetcher = new XMLHttpRequest();
+    route_fetcher.open("GET", "https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=o-9q5-metro~losangeles&limit=700&route_type=3&include_alerts=true");
+    route_fetcher.onreadystatechange = function() {
+        if (route_fetcher.readyState === 4 && route_fetcher.status === 200) {
+            var route_info = JSON.parse(route_fetcher.responseText);
+            
+            for (var i=0; i<route_info.routes.length; i++) {
+                var route_short_name = route_info.routes[i].route_short_name;
+                var route_long_name = route_info.routes[i].route_long_name;
+                var route_color = route_info.routes[i].route_color;
+                var route_text_color = route_info.routes[i].route_text_color;
+
+                if (route_short_name === "") {
+                    route_short_name = "&nbsp;&nbsp;&nbsp;";
+                }
+
+                if (route_info.routes[i].alerts.length === 0) {
+                    document.getElementById("no_of_alerts_bus").innerHTML = "";
+                }
+                else {
+                    document.getElementById("no_of_alerts_bus").innerHTML = `(<i class="fa-solid fa-triangle-exclamation"></i> ${route_info.routes[i].alerts.length})`;
+                }
+
+                document.getElementById("route_name_bus").innerHTML = route_short_name;
+                document.getElementById("route_name_bus").style.color = `#${route_text_color}`;
+                document.getElementById("route_name_bus").style.backgroundColor = `#${route_color}40`;
+                document.getElementById("route_name_bus").style.border = `1px solid #${route_color}`;
+                document.getElementById("route_desc_bus").innerHTML = route_long_name;
+
+                var route_entity = document.getElementById("route_item_bus");
+                var clone_entity = route_entity.cloneNode(true);
+                document.querySelector(".lametro_bus").appendChild(clone_entity);
+            }
+
+            var all_bus_lines = document.querySelector(".lametro_bus").children;
+            document.querySelector(".lametro_bus").removeChild(all_bus_lines[0]);
+        }
+    }
+    route_fetcher.send();
+}
+loadLAMetroLinesBus();
