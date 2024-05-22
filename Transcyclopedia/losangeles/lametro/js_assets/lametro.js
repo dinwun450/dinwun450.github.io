b7cfb942cd291552683a98c93ab0a72bd6bed751
Dino Wun <mightygboole1815@gmail.com>
2024-05-22 08:01:37 -0700

    Fix issues in adding new bus icons for LA Metro in lines page, p5

--- Transcyclopedia/losangeles/lametro/js_assets/lametro.js
+++ Transcyclopedia/losangeles/lametro/js_assets/lametro.js
@@ -124,10 +124,6 @@ function loadLAMetroLinesBus() {
                 var route_desc = route_info.routes[i].route_desc;
                 var corr_image = la_metro_img_icons[route_long_name];
 
-                if (route_short_name === "") {
-                    document.getElementById("route_name_bus").innerHTML = `<img src="${corr_image}" id="la_metro_bus_img" style="width: 20px; height: 20px; display: block; margin-left: auto; margin-right: auto;">`;
-                }
-
                 if (route_info.routes[i].alerts.length === 0) {
                     document.getElementById("no_of_alerts_bus").innerHTML = "";
                 }
@@ -143,7 +139,13 @@ function loadLAMetroLinesBus() {
                     route_text_color = "ffffff"
                 }
 
-                document.getElementById("route_name_bus").innerHTML = route_short_name;
+                if (route_short_name === "") {
+                    document.getElementById("route_name_bus").innerHTML = `<img src="${corr_image}" id="la_metro_bus_img" style="width: 20px; height: 20px; display: block; margin-left: auto; margin-right: auto;">`;
+                }
+                else {
+                    document.getElementById("route_name_bus").innerHTML = route_short_name;
+                }
+
                 document.getElementById("route_name_bus").style.color = `#${route_text_color}`;
                 document.getElementById("route_name_bus").style.backgroundColor = `#${route_color}40`;
                 document.getElementById("route_name_bus").style.border = `1px solid #${route_color}`;
