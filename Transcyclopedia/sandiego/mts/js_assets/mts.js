ea39b9b752154bf225c7bc21a0a491cce01338b8
Dino Wun <mightygboole1815@gmail.com>
2024-08-18 16:33:53 -0700

    Move Map Configuration function to JS file.
    Then update the coordinates for the MTS page. It's in San Diego.

--- Transcyclopedia/sandiego/mts/js_assets/mts.js
+++ Transcyclopedia/sandiego/mts/js_assets/mts.js
@@ -445,3 +445,72 @@ function loadMTSAlertsByRoutes() {
     mts_route_alert_caller.send();
 }
 loadMTSAlertsByRoutes();
+
+function mapConfiguration() {
+    document.getElementById("map").style.width = "100%";
+    document.getElementById("map").style.height = "500px";
+
+    const mapDiv = document.getElementById("map");
+    if (mapDiv.style.visibility === false) {
+        map.resize();
+    };
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGlud3VuNDUwIiwiYSI6ImNsaTR4c253bTAzNnIzcnF1aTh0eTN2a3kifQ.fjcqiGi68hyGpUAKAo9Tcw';
+    map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v11',
+        center: [-117.15726, 32.71533],
+        zoom: 8
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+    map.scrollZoom.disable();
+
+    map.on('style.load', () => {
+        // Insert the layer beneath any symbol layer.
+        const layers = map.getStyle().layers;
+        const labelLayerId = layers.find(
+            (layer) => layer.type === 'symbol' && layer.layout['text-field']
+        ).id;
+        
+        // The 'building' layer in the Mapbox Streets
+        // vector tileset contains building height data
+        // from OpenStreetMap.
+        map.addLayer({
+                    'id': 'add-3d-buildings',
+                    'source': 'composite',
+                    'source-layer': 'building',
+                    'filter': ['==', 'extrude', 'true'],
+                    'type': 'fill-extrusion',
+                    'minzoom': 15,
+                    'paint': {
+                    'fill-extrusion-color': '#aaa',
+                    
+                    // Use an 'interpolate' expression to
+                    // add a smooth transition effect to
+                    // the buildings as the user zooms in.
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'height']
+                    ],
+                    'fill-extrusion-base': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'min_height']
+                    ],
+                    'fill-extrusion-opacity': 0.6
+                }
+            },
+            labelLayerId
+        );
+    });
+}
