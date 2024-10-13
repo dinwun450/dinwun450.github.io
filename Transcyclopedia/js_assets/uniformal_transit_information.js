d6f4423c097e505b7355c6de03b3f7e80e8bc036
Dino Wun <mightygboole1815@gmail.com>
2024-10-12 23:53:40 -0700

    Fix issue in Multimodal Transit Function in UTI

--- Transcyclopedia/js_assets/uniformal_transit_information.js
+++ Transcyclopedia/js_assets/uniformal_transit_information.js
@@ -725,7 +725,7 @@ function plotLinesWithMultipleModesFromAnAgency(onestop_id, ins_type_one, ins_ty
                     },
                     'generateId': true
                 });
-                if (ins_type_one === 3) {
+                if (ins_type_three === 3) {
                     map.addLayer({
                         'id': 'third_line_routes',
                         'type': 'line',
