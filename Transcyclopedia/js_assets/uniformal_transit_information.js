ef9ff2cf9885df875e4824bbc36f18225512fd63
Dino Wun <mightygboole1815@gmail.com>
2024-10-12 23:50:36 -0700

    Add Multimodal transit function map in UTI

--- Transcyclopedia/js_assets/uniformal_transit_information.js
+++ Transcyclopedia/js_assets/uniformal_transit_information.js
@@ -597,3 +597,450 @@ function plotLinesFromAnAgency(onestop_id, ins_singular_route_type) {
     }
     plot_lines_caller.send();
 }
+
+function plotLinesWithMultipleModesFromAnAgency(onestop_id, ins_type_one, ins_type_two, ins_type_three) {
+    var plot_lines_caller = new XMLHttpRequest();
+    plot_lines_caller.open("GET", `https://transit.land/api/v2/rest/routes?api_key=x5unflDSbpKEWnThyfmteM8MHxIsg3eL&operator_onestop_id=${onestop_id}&limit=700&include_geometry=true`);
+    plot_lines_caller.onreadystatechange = function() {
+        if (plot_lines_caller.readyState === 4 && plot_lines_caller.status === 200) {
+            var plot_lines_receiver = JSON.parse(plot_lines_caller.responseText);
+            for (var i = 0; i < plot_lines_receiver.routes.length; i++) {
+                var route_geometry = plot_lines_receiver.routes[i].geometry;
+                var route_color = plot_lines_receiver.routes[i].route_color;
+                var route_text_color = plot_lines_receiver.routes[i].route_text_color;
+                var route_short_name = plot_lines_receiver.routes[i].route_short_name;
+                var route_long_name = plot_lines_receiver.routes[i].route_long_name;
+                var route_id = plot_lines_receiver.routes[i].route_id;
+                var route_type = plot_lines_receiver.routes[i].route_type;
+
+                geojson_of_routes.push({
+                    "type": "Feature",
+                    "properties": {
+                        "route_id": route_id,
+                        "route_short_name": route_short_name,
+                        "route_long_name": route_long_name,
+                        "route_color": `#${route_color}`,
+                        "route_text_color": `#${route_text_color}`,
+                        "route_id": route_id,
+                        "route_type": route_type
+                    },
+                    "geometry": route_geometry
+                });
+            }
+
+            var filtered_one_route = geojson_of_routes.filter((route) => route.properties.route_type === ins_type_one);
+            map.addSource('first_source_route', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': filtered_one_route
+                },
+                'generateId': true
+            });
+            if (ins_type_one === 3) {
+                map.addLayer({
+                    'id': 'first_line_routes',
+                    'type': 'line',
+                    'source': 'first_source_route',
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                        'line-width': 2,
+                        'line-color': [
+                            'case',
+                            ['boolean', ['feature-state', 'hover'], false],
+                            '#c0e7fc'
+                        ],
+                    }
+                });
+            }
+            else {
+                map.addLayer({
+                    'id': 'first_line_routes',
+                    'type': 'line',
+                    'source': 'first_source_route',
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                        'line-width': 4,
+                        'line-color': ['get', 'route_color']
+                    }
+                });
+            }
+            var filtered_two_route = geojson_of_routes.filter((route) => route.properties.route_type === ins_type_two);
+            map.addSource('second_source_route', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': filtered_two_route
+                },
+                'generateId': true
+            });
+            if (ins_type_one === 3) {
+                map.addLayer({
+                    'id': 'second_line_routes',
+                    'type': 'line',
+                    'source': 'second_source_route',
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                        'line-width': 2,
+                        'line-color': [
+                            'case',
+                            ['boolean', ['feature-state', 'hover'], false],
+                            '#c0e7fc'
+                        ],
+                    }
+                });
+            }
+            else {
+                map.addLayer({
+                    'id': 'second_line_routes',
+                    'type': 'line',
+                    'source': 'second_source_route',
+                    'layout': {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    'paint': {
+                        'line-width': 4,
+                        'line-color': ['get', 'route_color']
+                    }
+                });
+            }
+
+            if (ins_type_three !== null) {
+                var filtered_three_route = geojson_of_routes.filter((route) => route.properties.route_type === ins_type_three);
+                map.addSource('third_source_route', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': filtered_three_route
+                    },
+                    'generateId': true
+                });
+                if (ins_type_one === 3) {
+                    map.addLayer({
+                        'id': 'third_line_routes',
+                        'type': 'line',
+                        'source': 'third_source_route',
+                        'layout': {
+                            'line-join': 'round',
+                            'line-cap': 'round'
+                        },
+                        'paint': {
+                            'line-width': 2,
+                            'line-color': [
+                                'case',
+                                ['boolean', ['feature-state', 'hover'], false],
+                                '#c0e7fc'
+                            ],
+                        }
+                    });
+                }
+                else {
+                    map.addLayer({
+                        'id': 'third_line_routes',
+                        'type': 'line',
+                        'source': 'third_source_route',
+                        'layout': {
+                            'line-join': 'round',
+                            'line-cap': 'round'
+                        },
+                        'paint': {
+                            'line-width': 4,
+                            'line-color': ['get', 'route_color']
+                        }
+                    });
+                }
+            }
+            else {
+                var filtered_three_route = null;
+            }
+
+            map.moveLayer('first_line_routes', 'second_line_routes', 'third_line_routes');
+
+            map.on('movestart', () => {
+                map.setFilter('first_line_routes', ['has', 'route_id']);
+                map.setFilter('second_line_routes', ['has', 'route_id']);
+                map.setFilter('third_line_routes', ['has', 'route_id']);
+
+                document.getElementById("range_of_routes").innerHTML = `
+                    <li id="rir" class="route_radius"><span id="route_name_rad" class="styling_for_routes">-</span> <span id="route_detail_rad">Loading...</span></li>
+                `;
+            });
+
+            map.on('moveend', () => {
+                const features = map.queryRenderedFeatures({
+                    layers: ['first_line_routes', 'second_line_routes', 'third_line_routes']
+                });
+
+                if (features) {
+                    const uniqueFeatures = getUniqueFeatures(features, 'route_id');
+
+                    for (var f=0; f<uniqueFeatures.length; f++) {
+                        var name_of_route = uniqueFeatures[f].properties.route_short_name;
+                        var desc_of_route = uniqueFeatures[f].properties.route_long_name;
+                        var color_of_route = uniqueFeatures[f].properties.route_color;
+                        var text_color_of_route = uniqueFeatures[f].properties.route_text_color;
+
+                        if (name_of_route === "") {
+                            document.getElementById("route_name_rad").innerHTML = `&nbsp;&nbsp;&nbsp;`;
+                        } else {
+                            document.getElementById("route_name_rad").innerHTML = name_of_route;
+                        }
+
+                        document.getElementById("route_name_rad").style.backgroundColor = `${color_of_route}40`;
+                        document.getElementById("route_name_rad").style.color = `${text_color_of_route}`;
+                        document.getElementById("route_name_rad").style.border = `1px solid ${color_of_route}`;
+                        document.getElementById("route_detail_rad").innerHTML = desc_of_route;
+
+                        var each_route_entity = document.getElementById("rir").cloneNode(true);
+                        document.getElementById("range_of_routes").appendChild(each_route_entity);
+                    };
+
+                    var all_routes = document.getElementById("range_of_routes").children;
+                    document.getElementById("range_of_routes").removeChild(all_routes[0]);
+                }
+            });
+
+            let routeId = [];
+            let routeId_bus_only = [];
+            let hoverId_for_bus = [];
+            let hoveredPolygonLine = null;
+
+            const popup = new mapboxgl.Popup({
+                closeButton: false,
+                closeOnClick: false
+            });
+
+            if (ins_type_one === 3) {
+                map.on('mouseenter', 'first_line_routes', (e) => {
+                    var fs = map.queryRenderedFeatures({
+                        layers: ['first_line_routes']
+                    });
+
+                    if (fs.length > 0) {
+                        for (var f=0; f<fs.length; f++) {
+                            var name_of_route = fs[f].properties.route_short_name;
+                            routeId_bus_only.push(name_of_route);
+
+                            hoveredPolygonLine = fs[f].id;
+                            hoverId_for_bus.push(hoveredPolygonLine);
+
+                            if (hoveredPolygonLine !== null) {
+                                map.setFeatureState(
+                                    { source: 'first_source_route', id: hoveredPolygonLine },
+                                    { hover: false }
+                                );
+                            }
+                            map.setFeatureState(
+                                { source: 'first_source_route', id: hoveredPolygonLine },
+                                { hover: true }
+                            );
+                        }
+
+                        popup.setLngLat(e.lngLat.wrap()).setHTML(routeId_bus_only).addTo(map);
+                    }
+                });
+
+                map.on('mouseleave', 'first_line_routes', () => {
+                    if (routeId_bus_only.length > 0) {
+                        routeId_bus_only = [];
+                    }
+                    popup.remove();
+
+                    if (hoveredPolygonLine !== null) {
+                        for (var i = 0; i < hoverId_for_bus.length; i++) {
+                            map.setFeatureState(
+                                { source: 'first_source_route', id: hoverId_for_bus[i] },
+                                { hover: false }
+                            );
+                        }
+                    }
+                    hoveredPolygonLine = null;
+                });
+            }
+            else {
+                map.on('mouseenter', 'first_line_routes', (e) => {
+                    var fs = map.queryRenderedFeatures(e.point, {
+                        layers: ['first_line_routes']
+                    });
+
+                    if (fs.length > 0) {
+                        for (var f=0; f<fs.length; f++) {
+                            var name_of_route = fs[f].properties.route_short_name;
+                            if (name_of_route === "") {
+                                name_of_route = fs[f].properties.route_long_name;
+                            }
+                        }
+
+                        popup.setLngLat(e.lngLat.wrap()).setHTML(routeId).addTo(map);
+                    }
+                });
+
+                map.on('mouseleave', 'first_line_routes', () => {
+                    if (routeId.length > 0) {
+                        routeId = [];
+                    }
+                    popup.remove();
+                });
+            }
+
+            if (ins_type_two === 3) {
+                map.on('mouseenter', 'second_line_routes', (e) => {
+                    var fs2 = map.queryRenderedFeatures({
+                        layers: ['second_line_routes']
+                    });
+
+                    if (fs2.length > 0) {
+                        for (var f2=0; f2<fs2.length; f2++) {
+                            var name_of_route = fs2[f2].properties.route_short_name;
+                            routeId_bus_only.push(name_of_route);
+
+                            hoveredPolygonLine = fs2[f2].id;
+                            hoverId_for_bus.push(hoveredPolygonLine);
+
+                            if (hoveredPolygonLine !== null) {
+                                map.setFeatureState(
+                                    { source: 'second_source_route', id: hoveredPolygonLine },
+                                    { hover: false }
+                                );
+                            }
+                            map.setFeatureState(
+                                { source: 'second_source_route', id: hoveredPolygonLine },
+                                { hover: true }
+                            );
+                        }
+
+                        popup.setLngLat(e.lngLat.wrap()).setHTML(routeId_bus_only).addTo(map);
+                    }
+                });
+
+                map.on('mouseleave', 'second_line_routes', () => {
+                    if (routeId_bus_only.length > 0) {
+                        routeId_bus_only = [];
+                    }
+                    popup.remove();
+
+                    if (hoveredPolygonLine !== null) {
+                        for (var i2 = 0; i2 < hoverId_for_bus.length; i2++) {
+                            map.setFeatureState(
+                                { source: 'second_source_route', id: hoverId_for_bus[i2] },
+                                { hover: false }
+                            );
+                        }
+                    }
+                    hoveredPolygonLine = null;
+                });
+            }
+            else {
+                map.on('mouseenter', 'second_line_routes', (e) => {
+                    var fs2 = map.queryRenderedFeatures(e.point, {
+                        layers: ['second_line_routes']
+                    });
+
+                    if (fs2.length > 0) {
+                        for (var f2=0; f2<fs2.length; f2++) {
+                            var name_of_route = fs2[f2].properties.route_short_name;
+                            if (name_of_route === "") {
+                                name_of_route = fs2[f2].properties.route_long_name;
+                            }
+                        }
+
+                        popup.setLngLat(e.lngLat.wrap()).setHTML(routeId).addTo(map);
+                    }
+                });
+
+                map.on('mouseleave', 'second_line_routes', () => {
+                    if (routeId.length > 0) {
+                        routeId = [];
+                    }
+                    popup.remove();
+                });
+            }
+
+            if (ins_type_three !== null) {
+                if (ins_type_three === 3) {
+                    map.on('mouseenter', 'third_line_routes', (e) => {
+                        var fs3 = map.queryRenderedFeatures({
+                            layers: ['third_line_routes']
+                        });
+
+                        if (fs3.length > 0) {
+                            for (var f3=0; f3<fs3.length; f3++) {
+                                var name_of_route = fs3[f3].properties.route_short_name;
+                                routeId_bus_only.push(name_of_route);
+
+                                hoveredPolygonLine = fs3[f3].id;
+                                hoverId_for_bus.push(hoveredPolygonLine);
+
+                                if (hoveredPolygonLine !== null) {
+                                    map.setFeatureState(
+                                        { source: 'third_source_route', id: hoveredPolygonLine },
+                                        { hover: false }
+                                    );
+                                }
+                                map.setFeatureState(
+                                    { source: 'third_source_route', id: hoveredPolygonLine },
+                                    { hover: true }
+                                );
+                            }
+
+                            popup.setLngLat(e.lngLat.wrap()).setHTML(routeId_bus_only).addTo(map);
+                        }
+                    });
+
+                    map.on('mouseleave', 'third_line_routes', () => {
+                        if (routeId_bus_only.length > 0) {
+                            routeId_bus_only = [];
+                        }
+                        popup.remove();
+
+                        if (hoveredPolygonLine !== null) {
+                            for (var i3 = 0; i3 < hoverId_for_bus.length; i3++) {
+                                map.setFeatureState(
+                                    { source: 'third_source_route', id: hoverId_for_bus[i3] },
+                                    { hover: false }
+                                );
+                            }
+                        }
+                        hoveredPolygonLine = null;
+                    });
+                }
+                else {
+                    map.on('mouseenter', 'third_line_routes', (e) => {
+                        var fs3 = map.queryRenderedFeatures(e.point, {
+                            layers: ['third_line_routes']
+                        });
+
+                        if (fs3.length > 0) {
+                            for (var f3=0; f3<fs3.length; f3++) {
+                                var name_of_route = fs3[f3].properties.route_short_name;
+                                if (name_of_route === "") {
+                                    name_of_route = fs3[f3].properties.route_long_name;
+                                }
+                            }
+
+                            popup.setLngLat(e.lngLat.wrap()).setHTML(routeId).addTo(map);
+                        }
+                    });
+
+                    map.on('mouseleave', 'third_line_routes', () => {
+                        if (routeId.length > 0) {
+                            routeId = [];
+                        }
+                        popup.remove();
+                    }
+                )};
+            }
+        }
+    }
+    plot_lines_caller.send();
+}
